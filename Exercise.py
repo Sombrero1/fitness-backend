dfbecbf3dd123209bc9a495b7ed42a8b1f198cc2
Vladimir Shevchenko <sombrero1@yandex-team.ru>
2022-11-20 17:54:55 +0300

    class -> class_exercise

--- Exercise.py
+++ Exercise.py
@@ -9,7 +9,7 @@ class User(Document):
 class Exercise(Document):
     name = StringField(required=True, max_length=30, unique=True)
     description = StringField(max_length=500)
-    type = StringField(db_field='class', max_length=20)
+    class_exercise = StringField(max_length=20)
     measure = StringField()
     count = IntField(min_value=0)
     picture = StringField() #base64
